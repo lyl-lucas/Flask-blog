74fda6ce6fa7a2e3f2f9a61d4eeaa98c9cb176ec
LLucas <lyl.lucas@gmail.com>
2016-09-08 09:24:42 +0800

    moment+wtf+email

--- app.py
+++ app.py
@@ -1,23 +1,123 @@
-from flask import Flask
-from flask import make_response
+from flask import Flask, redirect, url_for, session, flash
 from flask.ext.script import Manager
 from flask import render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.mail import Mail, Message
+import os
+from threading import Thread
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY'] = 'hard to guess code'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASK_ADMIN'] = os.environ.get('FLASK_ADMIN')
+app.config['FLASK_MAIL_SENDER'] = 'FLASK_MAIL_SENDER <lyl.lucas@gmail.com>'
+
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+mail = Mail(app)
 
 
-@app.route('/')
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message('[Flask]' + subject,
+                  sender=app.config['FLASK_MAIL_SENDER'],
+                  recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.body = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+from flask.ext.script import Shell
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+manager.add_command('import_data',
+                    Shell(make_context=make_shell_context))
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    resp = make_response(render_template('index.html'))
-    resp.set_cookie('answer', '42')
-    return resp
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASK_ADMIN']:
+                send_email(app.config['FLASK_ADMIN'],
+                           'new user', 'mail/new_user',
+                           user=user)
+        else:
+            session['known'] = True
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('You are not the one who came last moment.')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form,
+                           name=session.get('name'),
+                           known=session.get('known')
+                           )
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name,
+                           current_time=datetime.utcnow())
 
 
 @app.errorhandler(404)
